33650e119a3f93b9436ca0601a9c7616e7ca72a7
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-07-11 11:08:28 -0500

    Initialize project on spring initalizr

---
+++ src/main/java/edu/miu/cs425/gameshackhutapp/GameshackhutappApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.gameshackhutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshackhutappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshackhutappApplication.class, args);
+	}
+
+}
